cc797e9c1e18edc534b02630e25f9b6b050bdb94
5482MIsha <ivanov.3005misha@gmail.com>
2025-07-21 17:22:38 +0300

    Add some more text

--- group-9/App.js
+++ group-9/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!123</Text>
+      <Text>Open up App.js to start working on your app!123 rsgln</Text>
       <StatusBar style="auto" />
     </View>
   );
